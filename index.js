3cb69e4dcbf069df590857546317cd31ac914c16
Claritza <claritza.medina220@hotmail.com>
2020-06-24 10:35:07 -0400

    adding files

---
+++ index.js
@@ -0,0 +1,12 @@
+var express = require("express");
+
+var app = express();
+var PORT = 3000;
+
+app.get("/", function (req, res) {
+  res.send("Hello");
+});
+
+app.listen(PORT, function () {
+  console.log("App listening on PORT " + PORT);
+});
